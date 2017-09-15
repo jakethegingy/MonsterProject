f4c329f1f5a8fa72ba4b0e8f10934d9d9e308681
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-09-15 09:33:00 -0600

    added a interactive thingy
    added some context and how to respond to the machine

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,7 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController 
 {
@@ -8,6 +9,23 @@ public class MonsterController
 	{
 		MarshmallowMonster sample = new MarshmallowMonster();
 		System.out.println(sample);
-		MarshmallowMonster realMonster = new MarshmallowMonster("Scary", 8, 7, 5, true);
+		MarshmallowMonster realMonster = new MarshmallowMonster("Jaquabious", 5, 695591, 5, true);
+		System.out.println(realMonster);
+		System.out.println("Jaquabious is very hangry. He is going to eat is own tensticle");
+		realMonster.setTentacleAmount(5);
+		System.out.println(realMonster);
+		
+		interactWithTheMonster(realMonster);
+	}
+	
+	private void interactWithTheMonster(MarshmallowMonster currentMonster)
+	{
+		System.out.println(currentMonster.getName() + " wants to know what to eat next");
+		System.out.println(currentMonster.getName() + " suggests arms, they have " + currentMonster.getArmCount());
+		System.out.println("How many do you want to eat?");
+		Scanner myScanner = new Scanner(System.in);
+		int consumed = myScanner.nextInt();
+		currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+		System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
 	}
 }
